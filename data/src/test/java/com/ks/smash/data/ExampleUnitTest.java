344def2b62a00f29ba271c82ce02dd40b46df07b
Jieyi <pokkbaby@gmail.com>
2018-03-04 12:05:14 +0900

    Added the clean architecture and separated three modules.

--- data/src/test/java/com/ks/smash/data/ExampleUnitTest.java
+++ data/src/test/java/com/ks/smash/data/ExampleUnitTest.java
@@ -16,5 +16,18 @@
 
 package com.ks.smash.data;
 
-public class MyClass {
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
 }
