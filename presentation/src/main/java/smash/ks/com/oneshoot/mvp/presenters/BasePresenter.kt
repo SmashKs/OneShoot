62361b8e532cf7b8ab518a2f0724b9192e0a87d4
Jieyi <pokkbaby@gmail.com>
2018-03-06 00:12:58 +0900

    Added mvp architecture

---
+++ presentation/src/main/java/smash/ks/com/oneshoot/mvp/presenters/BasePresenter.kt
@@ -0,0 +1,40 @@
+/*
+ * Copyright (C) 2018 The Smash Ks Open Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package smash.ks.com.oneshoot.mvp.presenters
+
+import smash.ks.com.oneshoot.mvp.views.MvpView
+
+abstract class BasePresenter<V : MvpView> : MvpPresenter {
+    open lateinit var view: V
+//    protected lateinit var lifecycleProvider: LifecycleProvider<*>
+
+//    override fun <E> create(lifecycleProvider: LifecycleProvider<E>) {
+//        this.lifecycleProvider = lifecycleProvider
+//    }
+
+    override fun init() {}
+
+    override fun start() {}
+
+    override fun resume() {}
+
+    override fun pause() {}
+
+    override fun stop() {}
+
+    override fun destroy() {}
+}
---
+++ presentation/src/main/java/smash/ks/com/oneshoot/mvp/views/MvpView.kt
@@ -0,0 +1,49 @@
+/*
+ * Copyright (C) 2018 The Smash Ks Open Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package smash.ks.com.oneshoot.mvp.views
+
+/**
+ * Interface representing a View that will use to load data.
+ */
+interface MvpView {
+    /**
+     * Show a view with a progress bar indicating a loading process.
+     */
+    fun showLoading()
+
+    /**
+     * Hide a loading view.
+     */
+    fun hideLoading()
+
+    /**
+     * Show a retry view in case of an error when retrieving data.
+     */
+    fun showRetry()
+
+    /**
+     * Hide a retry view shown if there was an error when retrieving data.
+     */
+    fun hideRetry()
+
+    /**
+     * Show an error message
+     *
+     * @param message A string representing an error.
+     */
+    fun showError(message: String)
+}
