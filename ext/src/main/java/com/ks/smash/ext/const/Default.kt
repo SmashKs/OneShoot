862ea8d04d56991a7bef2100300882175917fa5c
Jieyi <pokkbaby@gmail.com>
2018-03-20 18:27:16 +0900

    Added the checking default value.

--- ext/src/main/java/com/ks/smash/ext/const/Default.kt
+++ ext/src/main/java/com/ks/smash/ext/const/Default.kt
@@ -14,9 +14,18 @@
  * limitations under the License.
  */
 
+@file:Suppress("NOTHING_TO_INLINE")
+
 package com.ks.smash.ext.const
 
 const val DEFAULT_INT = -1
 const val DEFAULT_LONG = -1L
 const val DEFAULT_DOUBLE = -1.0
+const val DEFAULT_FLOAT = -1f
 const val DEFAULT_STR = ""
+
+inline fun Int.isDefault() = DEFAULT_INT == this
+inline fun Long.isDefault() = DEFAULT_LONG == this
+inline fun String.isDefault() = DEFAULT_STR == this
+inline fun Double.isDefault() = DEFAULT_DOUBLE == this
+inline fun Float.isDefault() = DEFAULT_FLOAT == this
