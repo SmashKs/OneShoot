659a1a2b384a99c8ccb3532b861d629c75187755
Jieyi <pokkbaby@gmail.com>
2018-04-18 14:42:16 +0900

    Prepared for changing gradle to gradle.kts.

--- buildSrc/source/main/java/resources/Metal.kt
+++ buildSrc/source/main/java/resources/Metal.kt
@@ -14,31 +14,10 @@
  * limitations under the License.
  */
 
-/*
- * Copyright (C) 2018 The Smash Ks Open Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *       http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+package resources
 
 object Metal {
     val dirs = arrayListOf(
-        "src/main/res-features/metal",
-        "src/main/res-features/metal/benefits",
-        "src/main/res-features/metal/benefits/list",
-        "src/main/res-features/metal/benefits/details",
-        "src/main/res-features/metal/claims",
-        "src/main/res-features/metal/claims/list",
-        "src/main/res-features/metal/claims/details",
-        "src/main/res-features/metal/membership"
+        "src/main/res-features/ks"
     )
 }
