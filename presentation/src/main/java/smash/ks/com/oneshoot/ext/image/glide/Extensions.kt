485b4b955fad1de314ab92bd8dcdbc2a6e60543b
Jieyi <pokkbaby@gmail.com>
2018-04-20 01:11:27 +0900

    Added an extension for glide.

---
+++ presentation/src/main/java/smash/ks/com/oneshoot/ext/image/glide/Extensions.kt
@@ -0,0 +1,84 @@
+/*
+ * Copyright (C) 2018 The Smash Ks Open Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package smash.ks.com.oneshoot.ext.image.glide
+
+import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.drawable.Drawable
+import android.net.Uri
+import android.support.annotation.DrawableRes
+import android.widget.ImageView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.Priority
+import com.bumptech.glide.RequestBuilder
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.bumptech.glide.request.RequestOptions
+import com.ks.smash.ext.const.DEFAULT_INT
+import smash.ks.com.oneshoot.ext.resource.gContext
+
+fun ImageView.loadByString(str: String, context: Context = gContext()) =
+    glideDefault(context) { load(str) }
+
+fun ImageView.loadByBitmap(bitmap: Bitmap, context: Context = gContext()) =
+    glideDefault(context) { load(bitmap) }
+
+fun ImageView.loadByUri(uri: Uri, context: Context = gContext()) =
+    glideDefault(context) { load(uri) }
+
+fun ImageView.loadByDrawable(drawable: Drawable, context: Context = gContext()) =
+    glideDefault(context) { load(drawable) }
+
+fun ImageView.loadByAny(any: Any, context: Context = gContext()) =
+    glideDefault(context) { load(any) }
+
+fun glideKsOptions(
+    @DrawableRes phResource: Int = DEFAULT_INT,
+    @DrawableRes erSource: Int = DEFAULT_INT
+) =
+    RequestOptions().apply {
+        centerCrop()
+        placeholder(phResource)
+        error(erSource)
+        priority(Priority.HIGH)
+        diskCacheStrategy(DiskCacheStrategy.RESOURCE)
+    }
+
+fun ImageView.glideDefault(
+    context: Context = gContext(),
+    block: RequestBuilder<Bitmap>.() -> RequestBuilder<Bitmap>
+) =
+    Glide.with(context)
+        .asBitmap()
+        .apply(glideKsOptions())
+        .block()
+        .into(this)
+
+fun glideObtaineBitmapFrom(uri: Uri, context: Context = gContext()) =
+    Glide.with(context)
+        .asBitmap()
+        .apply(glideKsOptions())
+        .load(uri)
+        .submit()
+        .get()
+
+fun glideObtaineDrawableFrom(uri: Uri, context: Context = gContext()) =
+    Glide.with(context)
+        .asDrawable()
+        .apply(glideKsOptions())
+        .load(uri)
+        .submit()
+        .get()
