3cadb051eca7e0bd10616351c8d68ff6c19bbb3c
Jieyi <pokkbaby@gmail.com>
2018-04-23 21:03:23 +0900

    Added the camera module.

---
+++ presentation/src/main/java/smash/ks/com/oneshoot/widgets/customize/camera/CameraView.kt
@@ -0,0 +1,62 @@
+/*
+ * Copyright (C) 2018 The Smash Ks Open Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package smash.ks.com.oneshoot.widgets.customize.camera
+
+import android.view.View
+
+abstract class CameraView(
+    protected val mCallback: Callback,
+    protected val mPreview: Preview
+) {
+    val view: View get() = mPreview.view
+
+    abstract val isCameraOpened: Boolean
+
+    abstract var facing: Int
+
+    abstract val supportedAspectRatios: Set<AspectRatio>
+
+    abstract val aspectRatio: AspectRatio
+
+    abstract var autoFocus: Boolean
+
+    abstract var flash: Int
+
+    /**
+     * @return `true` if the implementation was able to start the camera session.
+     */
+    abstract fun start(): Boolean
+
+    abstract fun stop()
+
+    /**
+     * @return `true` if the aspect ratio was changed.
+     */
+    abstract fun setAspectRatio(ratio: AspectRatio): Boolean
+
+    abstract fun takePicture()
+
+    abstract fun setDisplayOrientation(displayOrientation: Int)
+
+    interface Callback {
+        fun onCameraOpened()
+
+        fun onCameraClosed()
+
+        fun onPictureTaken(data: ByteArray)
+    }
+}
---
+++ presentation/src/main/java/smash/ks/com/oneshoot/widgets/customize/camera/Preview.kt
@@ -0,0 +1,50 @@
+/*
+ * Copyright (C) 2018 The Smash Ks Open Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package smash.ks.com.oneshoot.widgets.customize.camera
+
+import android.view.Surface
+import android.view.SurfaceHolder
+import android.view.View
+
+/**
+ * Encapsulates all the operations related to camera preview in a backward-compatible manner.
+ */
+abstract class Preview {
+    var onSurfaceChanged: (() -> Unit)? = null
+    var width = 0
+        private set
+    var height = 0
+        private set
+    var surfaceHolder: SurfaceHolder? = null
+    var surfaceTexture: Any? = null
+
+    abstract val surface: Surface
+    abstract val view: View
+    abstract val outputClass: Class<*>
+    abstract val isReady: Boolean
+
+    abstract fun setDisplayOrientation(displayOrientation: Int)
+
+    protected fun dispatchSurfaceChanged() = onSurfaceChanged?.invoke() ?: Unit
+
+    fun setBufferSize(width: Int, height: Int): Unit = TODO()
+
+    fun setSize(width: Int, height: Int) {
+        this.width = width
+        this.height = height
+    }
+}
