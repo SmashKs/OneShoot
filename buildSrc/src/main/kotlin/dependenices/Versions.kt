aaa0c593def88c6f85d3ca6789396ba7d7c1a0ac
Jieyi <pokkbaby@gmail.com>
2018-10-07 23:37:19 +0900

    Upgrade: upgraded the library version.

--- buildSrc/src/main/kotlin/dependenices/Versions.kt
+++ buildSrc/src/main/kotlin/dependenices/Versions.kt
@@ -36,18 +36,18 @@ object Versions {
      * Related Android component lib version.
      */
     object AndroidComponent {
-        const val androidx = "1.0.0-rc02"
+        const val androidx = "1.0.0"
         const val cardview = androidx
         const val constraintLayout = "2.0.0-alpha1"
         const val coordinatorLayout = androidx
-        const val navigation = "1.0.0-alpha05"
+        const val navigation = "1.0.0-alpha06"
     }
 
     /**
      * Related Kotlin lib version.
      */
     object Kotlin {
-        const val kotlinLib = "1.2.61"
+        const val kotlinLib = "1.2.71"
         const val kotlinCoroutine = "0.25.0"
     }
 
@@ -64,7 +64,7 @@ object Versions {
     object KotlinAndroidExt {
         const val dex = "2.0.0"
         const val aac = androidx
-        const val aacLifecycle = "2.0.0-rc01"
+        const val aacLifecycle = "2.0.0"
         const val anko = "0.10.5"
         const val ktx = androidx
         const val kinfer = "2.0.3"
